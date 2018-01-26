f7900ebce277d1ca2ce5b0f197919febb796c0c6
allenmatasy <allen.matasy@gmail.com>
2018-01-25 16:31:17 -0800

    Array of arrays practice

---
+++ arraysquared.js
@@ -0,0 +1,46 @@
+let playList = [
+  ['I Did It My Way', 'Frank Sinatra'],
+  ['Respect', 'Aretha Franklin'],
+  ['Imagine', 'John Lennon'],
+  ['Born to Run', 'Bruce Springsteen'],
+  ['Louie Louie', 'The Kingsmen'],
+  ['Maybellene', 'Chuck Berry']
+];
+
+function print(message) {
+  document.write(message);
+}
+
+function printSongs( songs ) {
+  let listHTML = '<ol>';
+  for ( let i = 0; i < songs.length; i += 1) {
+    listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
+  }
+  listHTML += '</ol>';
+  print(listHTML);
+}
+
+printSongs(playList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
