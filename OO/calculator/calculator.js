fe59ca026c5556195c0a45a503e3bfc34c1a01b1
allenmatasy <allen.matasy@gmail.com>
2018-02-23 10:55:49 -0800

    End of Part 1/4

--- OO/calculator/calculator.js
+++ OO/calculator/calculator.js
@@ -1,12 +1,21 @@
 let calculator = {
     sum: 0,
     add: function(value) {
-        this.sum += value;
+    this.sum += value;
+    },
+    subtract: function(value) {
+    this.sum -= value;
+    },
+    multiply: function(value) {
+    this.sum *= value;
+    },
+    divide: function(value) {
+    this.sum /= value;
     },
     clear: function() {
-        this.sum = 0;
-    }, 
+    this.sum = 0;
+    },
     equals: function() {
-        return this.sum;
+    return this.sum;
     }
-};
+}
